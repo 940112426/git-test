d8d62f6bfd72ee7641f1598362c4a8a713efbdc7
XuMingyue <940112426@qq.com>
2021-09-01 16:23:02 +0800

    git测试代码--first commit

---
+++ src/main/java/com/xmy/git/GitTest.java
@@ -0,0 +1,11 @@
+package com.xmy.git;
+
+/**
+ * @Author: XMY
+ * @createTime 2021-09-01
+ */
+public class GitTest {
+    public static void main(String[] args) {
+        System.out.println("hello Git");
+    }
+}
